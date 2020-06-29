7c332f97bc013356dd53a36573cfef05706f7b57
neda-karimi <nedziano@gmail.com>
2020-06-29 23:03:15 +0430

    mainActivity and manifest

--- app/src/main/java/com/example/moviefinder/MainActivity.java
+++ app/src/main/java/com/example/moviefinder/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.moviefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.nfc.Tag;
@@ -34,48 +35,66 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
     Context context;
+    private static final String msg = "MainACtivity";
     private JsonArrayRequest request;
     private RequestQueue requestQueue;
-    private List<MovieClass> lstmovie;
+    String url1 = "https://image.tmdb.org/t/p/W500/";
+    ArrayList<MovieClass> movieClasses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lstmovie = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+        gridLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
         posters = findViewById(R.id.film_poster);
         progressBar = findViewById(R.id.progress1);
-        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setHasFixedSize(true);
         getdata();
     }
 
+    private void view() {
+        adapter adapter = new adapter(MainActivity.this.movieClasses);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
+    }
+
     private void getdata() {
-        RequestQueue queue = Volley.newRequestQueue(this);
-       String url = "https://api.themoviedb.org/3/movie/popular?api_key=8e831e1fadddbac0c856db2f9bd078aa&language=en-US&page=1";
-        StringRequest stringRequest= new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+//        RequestQueue queue = Volley.newRequestQueue(this);
+        movieClasses = new ArrayList<>();
+        final String url = "https://api.themoviedb.org/3/movie/popular?api_key=8e831e1fadddbac0c856db2f9bd078aa&language=en-US&page=1";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(String response) {
+            public void onResponse(JSONArray response) {
                 progressBar.setVisibility(View.GONE);
-
-                try {
-
-                    JSONObject jsonObject = new JSONObject(response);
-                    //posters.setImageBitmap(jsonObject.getJSONArray("example"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                if (response != null) {
+                    Log.e(msg, "responsing:" + response);
+                    try {
+                        JSONArray jsonArray = response.getJSONArray(Integer.parseInt("results"));
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            try {
+                                JSONObject infor = jsonArray.getJSONObject(i);
+                                movieClasses.add(new MovieClass(url1 + infor.getString("poster_path"),  url1 + infor.getString("overview"),infor.getString("title")));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        view();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 progressBar.setVisibility(View.GONE);
-                Log.d("Error Response", error.toString());
-//            }
-//        }
-//        );
-//        queue.add(stringRequest);
+                Log.e(msg, "onResponse: " + error);
+
             }
         });
+
     }
 }
 
